97585023744005cf11c6ecec7a2bf4119dc0d3d2
fortin <fortin@ttu.ee>
2021-04-03 20:00:05 +0300

    Reading properly the data setting utf-8 encoding

--- README.md
+++ README.md
@@ -3,8 +3,4 @@
 
 ## Data aquisition
 
-Data is retrived following this [steps](tobeadded). For now I use only the CSV file called ```activities.csv```. where there is general data about the activities but in the zip file that can be downloaded there is a lot more. The file is not added in this repo for obvious reasons.
-
-### Comments
-
-For now if some description has EOL in the middle it sees as a new row causing problems in the reading. Need to fix that so it's not needed to change it manually
+Data is retrived following this [steps](https://support.strava.com/hc/en-us/articles/216918437-Exporting-your-Data-and-Bulk-Export). For now I use only the CSV file called ```activities.csv```. where there is general data about the activities but in the zip file that can be downloaded there is a lot more. The file is not added in this repo for obvious reasons.
--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 import sys
 import csv
 import os
+import traceback
+import re
 
 
 fd = os.path.sep  # folder delimiter
 fn = 'activities.csv'
 
+
 def reader(file_name):
     current_path = os.getcwd() + fd + str(file_name)
-    with open(current_path) as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=",")
-        print(csv_reader.fieldnames)
-        line_count = 0
-        try:
-            for row in csv_reader:
-                print(row)
-                line_count += 1
-        except UnicodeDecodeError: # catching unicode error. Still don't know the problem
-            pass
-    print('Columns read: {}'.format(line_count))
-    return csv_reader
+    result = []
+    try:
+        with open(current_path, encoding="utf8") as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=",")
+            field_names = csv_reader.fieldnames
+            line_count = 0
+            try:
+                for row in csv_reader:
+                    result.append(row)
+                    line_count += 1
+            except Exception:  # catching unicode error. Still don't know the problem
+                traceback.print_exc()
+                pass
+        print('Columns read: {}'.format(line_count))
+        return field_names, result
+    except FileNotFoundError:
+        print(('Please check that you have your file {}' +
+               ' in the same folder as this script').format(file_name))
+
+
+def total_distance(data):
+    """Total distance given in meters"""
+    total_distance = 0
+    for e in data:
+        total_distance += float(e['Distance'])
+    return total_distance
+
+
+def print_total_km(subset):
+    buffer = re.sub(r"\B(?=\d{3})+", " ",
+                    str(int(total_distance(subset)/1000)))
+    print('You have run {} km in total'.format(buffer))
 
 
 if __name__ == "__main__":
-    reader(fn)
+    fields, data = reader(fn)
+    print_total_km(data)
